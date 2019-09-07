9329d4b14972a7490f83367b163eeb8041be8a09
deezy13 <deezy13@gmail.com>
2019-09-07 01:09:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -87,11 +87,10 @@ end
 #///////////////////////////////////////////////////////////////////////////////
 music = ["rap", "jazz", "soul", "rocknRoll", "blues"]
 
-index_value_to_delete = 3
 
 def using_delete_at(array, indexToDelete = 0)
   array.delete(indexToDelete)
 end
   
-  using_delete_at(music, index_value_to_delete)
-  
+
+  var_created = using_delete_at(music, 4)
