a4ce76becffed49f1e93687d74b95c8a711d3725
deezy13 <deezy13@gmail.com>
2019-09-07 00:21:56 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,72 @@
+def using_push(array, string)
+    array.push(string)
+    end
+
+new_color = "red"
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+
+using_push(colors_in_the_rainbow, new_color)
+  
+ ########################################################################################
+  
+  
+ def using_unshift(array, string)
+  array.unshift(string)
+end
+
+
+#///////////////////////////////////////////////////////////////////////////////////////////
+
+def using_pop(array)
+  array.pop
+end
+
+#colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+#using_pop(colors_in_the_rainbow)
+
+
+#/////////////////////////////////////////////////////////////////////////////////////
+def pop_with_args(array)
+  array.pop(2);
+end
+
+#colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+#using_push(colors_in_the_rainbow)
+
+####/////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_cat(array1, array2)
+  array1.concat(array2)
+  end
+  
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+  end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delet_at(array, index_number)
+  array.delete(index_number)
+end
+  
+  
