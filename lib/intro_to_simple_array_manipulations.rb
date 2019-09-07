846c5f213d4cf132778253ff5bc868f17934ae20
deezy13 <deezy13@gmail.com>
2019-09-07 00:26:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -50,7 +50,7 @@ end
 #/////////////////////////////////////////////////////////////////////////////////////
 
 
-def using_cat(array1, array2)
+def using_concat(array1, array2)
   array1.concat(array2)
   end
   
